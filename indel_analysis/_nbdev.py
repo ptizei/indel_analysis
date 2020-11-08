dc325a48933cff8e3d3808a075b1e26f352b79e2
Pedro Tizei <ptizei@gmail.com>
2020-11-08 13:08:28 -0300

    getting tests to work

--- indel_analysis/_nbdev.py
+++ indel_analysis/_nbdev.py
@@ -2,9 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"trim_cterm": "00_readproc.ipynb"}
 
-modules = []
+modules = ["readproc.py"]
 
 doc_url = "https://fastai.github.io/indel_analysis/"
 
